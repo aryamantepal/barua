a8273d50acff1c0792be8d0a333b98966227790e
Aryaman Tepal <83552886+aryamantepal@users.noreply.github.com>
2022-03-08 09:54:28 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//if button is clicked, print hello
+
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+const h1 = document.querySelector('h1');
+ h1.textContent = 'Siddhant is from Nepal';
+ 
+
+ });
+
+ //if button is clicked, change image to NepalFlag
+    const img = document.querySelector('img');
+    button.addEventListener('click', function() {
+        img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Flag_of_Nepal.svg/1200px-Flag_of_Nepal.svg.png';
+    });
+    
+    
